4817c9c650d2b0a3020ae1b232b13558c7f8f2dd
Ivan Kuzmin <vania_kuzmin@mail.ru>
2018-07-13 14:58:03 +0300

    add icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 export default class Footer extends React.Component {
     render() {
         return (
             <View style={styles.footer}>
-                <View style={styles.footer_menu}></View>
-                <View style={styles.footer_menu}></View>
-                <View style={styles.footer_menu}></View>
+                <View style={styles.footer_menu}>
+                    <Icon name="text-document" size={25} color="white" />
+                </View>
+                <View style={styles.footer_menu}>
+                    <Icon name="location-pin" size={25} color="white" />
+                </View>
+                <View style={styles.footer_menu}>
+                    <Icon name="magnifying-glass" size={25} color="white" />
+                </View>
             </View>
         );
     }
@@ -17,11 +24,11 @@ const styles = StyleSheet.create({
     footer: {
         flex: 0.1,
         flexDirection: 'row'
-        // alignItems: 'center',
-        // justifyContent: 'flex-end'
     },
     footer_menu: {
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
