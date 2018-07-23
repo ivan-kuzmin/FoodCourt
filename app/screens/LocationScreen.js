767dceda02da105df008d80d91b37b41d3cde793
alexander-baranov <aleksrezv@gmail.com>
2018-07-23 19:59:16 +0300

    put all information to fairs.js

--- app/screens/LocationScreen.js
+++ app/screens/LocationScreen.js
@@ -1,104 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import MapView from 'react-native-maps';
-
-
-var places = [
-  {
-    coordinate: {
-      latitude: 55.78089562, 
-      longitude: 37.71965405
-    },
-    title: 'Семеноская ярмарка',
-    description: ''
-  }, 
-  {
-    coordinate: {
-      latitude: 55.7121779, 
-      longitude: 37.6206429
-    },
-    title: 'Даниловский рынок',
-    description: ''
-  },{
-    coordinate: {
-      latitude: 55.7274819, 
-      longitude: 37.5678689
-    },
-    title: 'Усачевский рынок',
-    description: ''
-  },{
-    coordinate: {
-      latitude: 55.77660849, 
-      longitude: 37.674848699999984
-    },
-    title: 'Гастроферма',
-    description: ''
-  },{
-    coordinate: {
-      latitude: 55.7443539, 
-      longitude: 37.679241
-    },
-    title: 'Рогожский рынок',
-    description: ''
-  },{
-    coordinate: {
-      latitude: 55.6305246, 
-      longitude: 37.51649169
-    },
-    title: 'Экомаркет в Коньково',
-    description: ''
-  },{
-    coordinate: {
-      latitude: 55.683952, 
-      longitude: 37.5505828
-    },
-    title: 'Черемушкинский рынок',
-    description: ''
-  },{
-    coordinate: {
-      latitude: 55.7668816, 
-      longitude: 37.6245211
-    },
-    title: 'Центральный рынок',
-    description: ''
-  }
-]
+import fairs from '../components/Fairs';
 
 export default class LocationScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          fairs: fairs
+        }
+      }
+
+
+    // componentWillMount() {
+    //   const fairs = 
+    // }
     render() {
         return (
             <View style={{flex: 1, backgroundColor: 'gray', flexDirection: 'column'}}>
-                {/* <StatusBar barStyle="light-content" /> */}
-                {/* <Header /> */}
-                {/* <Button
-                    title="Go to Details"
-                    onPress={() => this.props.navigation.navigate('Details')}
-                /> */}
                 <ScrollView style={{flex: 1}}>
                     <Text style={{color: "white", fontWeight: "bold"}}>LocationScreen</Text>
                 </ScrollView>
-                {/* <Footer /> */}
 
                 <View style={{flex:3}}>
                     <MapView style={styles.map} 
                         showsUserLocation  
-                        // followsUserLocation
                         initialRegion={{
                             latitude: 55.751244, 
                             longitude: 37.61842,
                             latitudeDelta: 0.8,
                             longitudeDelta: 0.8
                         }}> 
-                        {places.map(function(place, index){
+                        { this.state.fairs.map(function(fair, index){
                             return (
                                 <MapView.Marker
                                     key={index}
-                                    coordinate={place.coordinate}
-                                    title={place.title}
-                                    description={place.description}
+                                    coordinate={fair.content.coordinate}
+                                    title={fair.content.name}
+                                    description={fair.content.adress}
                                 />
                             )
-                        })}
+                          })
+                        }
                     </MapView>
                 </View>
             </View>
--- app/screens/TestScreen.js
+++ app/screens/TestScreen.js
@@ -1,76 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Button, Image } from 'react-native';
 import MainElement from '../components/MainElement';
+import PlaceElement from '../components/PlaceElement';
+import fairs from '../components/Fairs';
 
-{/*
-var places = [
-    {
-        'icon': '../assets/images/semenovskaya.jpg',
-        'name':' Place 1',
-        'description': 'Good place',
-        'adress': 'Semenovskaya, 1'
-    }, 
-    {
-        'icon': '../assets/images/danilovskiy.jpg',
-        'name': 'Place 2',
-        'description': 'Nice place',
-        'adress': 'Danilovskaya, 2'
+export default class TestScreen extends React.Component {
 
-    },
-    {
-        'icon': '../assets/images/usachevskiy.jpg',
-        'name': 'Place 3',
-        'description': 'Awful place',
-        'adress': 'Usachevskaya, 3'
-    } 
-]; 
-*/}
-
-
-class PlaceElement extends React.Component {
-    render() {
-        return (
-            <View style={{flexDirection: 'row', margin: 10}}>
-                <View style={{flex: 1}}>
-                    <Image
-                        style={{width: 50, height: 80}}
-                        // source={ 'require(\'' + this.props.icon + '\')'}
-                        source = {require('../assets/images/semenovskaya.jpg')}
-                    />
-                </View>
-                <View style={{flex: 4}}>
-                    <Text style={{fontWeight: 'bold', fontSize: 20}} > {this.props.name} </Text>
-                    <Text style={{fontStyle: 'italic'}}> {this.props.description} </Text>
-                    <Text style={{fontWeight: 'bold'}}> {this.props.adress} </Text>
-                </View>
-            </View>
-        )
+    constructor(props) {
+        super(props);
+        this.state = {
+          fairs: fairs
+        }
     }
-}
 
-export default class App extends React.Component {
     render() {
+        const { navigation } = this.props;
+        const fair_id = navigation.getParam('fair_id')
+        // console.log(fairs)
         return (
-            places.map(function(place, index){
+            fairs[fair_id].content.places.map(function(place, index){
                 return (
                     <PlaceElement key={index} style={styles.whole_place}
                         name={place.name}
                         description={place.description}
                         adress={place.adress}
+                        icon={place.icon}
                     />
-                ) 
-            })
+                )
+            }
+          )
         );
     };
 };
 
 const styles = StyleSheet.create({
     whole_place: {
-        flexDirection: 'row', 
+        flexDirection: 'row',
         margin: 10
     },
     place_image: {
-        
+
     },
     place_name: {
 
@@ -82,4 +51,3 @@ const styles = StyleSheet.create({
 
     }
 });
-
