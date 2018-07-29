49ff0a4ccd34850e1affc67bbba191b326a01d59
Ivan Kuzmin <vania_kuzmin@mail.ru>
2018-07-30 00:43:34 +0300

    add isLoading

--- app/components/MainElement.js
+++ app/components/MainElement.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, Image, Alert, TouchableHighlight } from "react-native";
+import { StyleSheet, Text, View, Button, Alert, TouchableHighlight } from "react-native";
+import Image from 'react-native-image-progress';
+import Progress from 'react-native-progress/CircleSnail';
 
 export default class MainElement extends React.Component {
+	state = {
+		isLoading: true
+	}
 	render() {
 		return (
 			<TouchableHighlight underlayColor="white" onPress={this.props.func}>
@@ -9,11 +14,14 @@ export default class MainElement extends React.Component {
 					<Image
                         style={{ width: "100%", height: "100%", position: "absolute" }}
                         source={{ uri: this.props.content.icon }}
+						indicator={Progress}
+						indicatorProps={{ color: "white" }}
+						onLoad={() => {this.setState({isLoading: false})}}
                         blurRadius={1}
                     />
-					<View style={styles.image_filter} />
-					<Text style={[styles.image_text, { fontSize: 25, textAlign: "center" }]}>{this.props.content.name}</Text>
-					<Text style={[styles.image_text, { fontWeight: "100", fontSize: 15, textAlign: "center" }]}>{this.props.content.adress}</Text>
+					<View style={[this.state.isLoading ? styles.hidden : {}, styles.image_filter]} />
+					<Text style={[this.state.isLoading ? styles.hidden : {}, styles.image_text, { fontSize: 25, textAlign: "center" }]}>{this.props.content.name}</Text>
+					<Text style={[this.state.isLoading ? styles.hidden : {}, styles.image_text, { fontWeight: "100", fontSize: 15, textAlign: "center" }]}>{this.props.content.adress}</Text>
 				</View>
 			</TouchableHighlight>
 		);
@@ -32,5 +40,9 @@ const styles = StyleSheet.create({
 		position: "absolute",
 		width: "100%",
 		height: "100%"
+	},
+	hidden: {
+		width: 0,
+		height: 0
 	}
 });
